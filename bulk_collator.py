ab02afd4670ac4005c615369023ac274eef32632
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2022-04-24 18:34:00 +0100

    Minimum working bulk collator utility script implemented.

---
+++ bulk_collator.py
@@ -0,0 +1,80 @@
+
+from openpyxl import Workbook
+from openpyxl.styles import Font
+from openpyxl.utils import get_column_letter
+from openpyxl.chart import BarChart, Reference
+from openpyxl import load_workbook
+
+import glob
+import os
+import subprocess
+import collections
+import statistics
+import argparse
+import logging
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("directory_list", metavar="directory_list", nargs='+', help="list of directories to collate together")
+    parser.add_argument("--generate_combined_spreadsheet", metavar="generate-combined-spreadsheet", type=bool, default=False, action=argparse.BooleanOptionalAction, help="generate spreadsheet of all collated marks")
+    parser.add_argument("--use_combined_spreadsheet", metavar="use-combined-spreadsheet", type=bool, default=False, action=argparse.BooleanOptionalAction, help="use combined spreadsheet to override marks from collated pdfs")
+    return parser.parse_args()
+
+# def generate_spreadsheet(args, authors: list[str], aliases: list[str], all_marks: list[list[MarkComment]], question_ids: list[str]):
+#     wb = Workbook()
+#     ws = wb.active
+#     wb.save(filename = os.path.join(os.getcwd(), args.input_dir, "extracted_marks.xlsx"))
+
+# def read_spreadsheet(args, authors, question_ids):
+#     wb = load_workbook(os.path.join(os.getcwd(), args.input_dir, "extracted_marks.xlsx"))
+#     ws = wb.active
+
+#     # read grid of marks
+#     overriding_marks: list[list[MarkComment]] = []
+#     for col in range(len(authors)):
+#         marks: list[MarkComment] = []
+#         for row in range(len(question_ids)):
+#             mark_comment = MarkComment(None)
+#             mark_comment.author = authors[col]
+#             mark_comment.question_id = question_ids[row]
+#             mark_comment.mark = ws.cell(row+4, col+3).value
+#             marks.append(mark_comment)
+#         overriding_marks.append(marks)
+
+#     return overriding_marks
+
+def main():
+
+    # set logging format
+    logging.basicConfig(format='%(asctime)s: %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
+
+    # extract agruments using argparse standard lib
+    args = get_arguments()
+
+    # validate against usage of override with and generate spreadsheet features together
+    if args.generate_combined_spreadsheet and args.use_combined_spreadsheet:
+        logging.error("Cannot use overriding spreadsheet and generate spreadsheet features at the same time!")
+        exit(-1)
+
+    # validate all collation directories are unique
+    if len(args.directory_list) != len(set(args.directory_list)):
+        logging.error("Collation directory list cannot contain duplicates!")
+        exit(-1)
+
+    # validate all collation directories exist
+    for directory in args.directory_list:
+        if not os.path.exists(os.path.join(os.getcwd(), directory)):
+            logging.error("Collation directory \"{}\" does not exist!".format(os.path.join(os.getcwd(), directory)))
+            exit(-1)
+
+    # Dev note: Calling of commands formed from user input is dangerous - should check/sanitise this...
+    for directory in args.directory_list:
+        logging.info("Collating {}.".format(directory))
+        command_string = "python collator.py {} {}.pdf".format(directory, os.path.basename(os.path.normpath(directory)))
+        return_code = subprocess.call(command_string, shell=True)
+        if return_code != 0:
+            logging.error("Collation of \"{}\" failed!".format(directory))
+            exit(-1)
+
+if __name__ == "__main__":
+    main()
